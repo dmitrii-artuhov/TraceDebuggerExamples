96f674f632c85c2d88a817a81db80a77933e8b95
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-11-13 18:22:25 +0100

    Add a subproject with enabled kover

---
+++ test-kover/src/test/kotlin/org/examples/kover/CalculatorTest.kt
@@ -0,0 +1,23 @@
+package org.examples.kover
+
+import org.examples.simple.Calculator
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class CalculatorTest {
+    private val calc = Calculator()
+
+    @Test
+    fun addTest() {
+        assertEquals(7, calc.add(3, 4))
+        assertEquals(-1, calc.subtract(3, 4))
+    }
+
+    @Test
+    fun isEvenTest() {
+        assertTrue(calc.isEven(2))
+        assertFalse(calc.isEven(3))
+    }
+}
